49c69e27b8c2477785a03a89503d035c4f138394
Davison <O.Davison3837@student.leedsbeckett.ac.uk>
2025-11-28 17:05:56 +0000

    [Django/HTML] - began process of making council_list and council_detail dynamic. restructure to supply_chain directory structure for organisation.

--- Django_Project/supply_chain/views.py
+++ Django_Project/supply_chain/views.py
@@ -1,22 +1,40 @@
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 
 from supply_chain.models import Council, Project
 
+
 def all_councils(request):
-    councils = Council.objects.all()
+    councils = (
+        Council.objects
+        .annotate(projects_count=Count('project'))
+        .order_by('name')
+    )
 
     context = {
         'councils': councils,
     }
 
-    return render(request, 'supply_chain/all_councils_list.html', context)
+    return render(request, 'supply_chain/council/all_councils_list.html', context)
+
 
 def all_projects(request):
-    return render(request, 'supply_chain/all_projects_list.html')
+    return render(request, 'supply_chain/project/all_projects_list.html')
+
 
 def project_detail(request, project_id):
-    return render(request, 'supply_chain/project_detail.html')
+    return render(request, 'supply_chain/project/project_detail.html')
 
 
 def council_detail(request, slug):
-    return render(request, 'supply_chain/council_detail.html')
+    council = get_object_or_404(
+        Council.objects.annotate(projects_count=Count('project')),
+        slug=slug
+    )
+
+    context = {
+        'council': council,
+        'project_count': council.projects_count,
+    }
+
+    return render(request, 'supply_chain/council/council_detail.html', context)
