ebfee7205bb2ccf25a186510ecdee6efbeb6d684
Oscar Davison <oscardavison8@outlook.com>
2025-11-28 23:26:03 +0000

    [Django] - changed url path for projects not to be the root of supply_chain but specifically routed to .../projects

--- Django_Project/supply_chain/urls.py
+++ Django_Project/supply_chain/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'supply_chain'
 
 urlpatterns = [
-    path('', views.all_projects, name='projects'),
+    path('projects', views.all_projects, name='projects'),
     path('councils', views.all_councils, name='councils'),
     path('projects/<int:project_id>', views.project_detail, name='project_detail'),
     path('councils/<slug:slug>', views.council_detail, name='council_detail')
