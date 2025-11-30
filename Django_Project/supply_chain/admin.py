a393768449f3bd9b522c6fcca16de28fa4af5c80
Oscar Davison <oscardavison8@outlook.com>
2025-11-30 17:05:24 +0000

    CHECKPOINT - content populated from database

--- Django_Project/supply_chain/admin.py
+++ Django_Project/supply_chain/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Council
+from .models import Council, Project
 
 
 class CouncilAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
+
 admin.site.register(Council, CouncilAdmin)
+admin.site.register(Project)
